b9b3e309f22da400fef4a440ec11957345db1b2a
naveenYm <naveen.somanna@yellowmessenger.com>
2021-03-25 15:34:12 +0530

    updated landing page

--- sidebars.js
+++ sidebars.js
@@ -35,7 +35,7 @@ module.exports = {
       items: [
         "howtos/basics/create-a-bot",
         "howtos/basics/signup-post-bot-invite",
-        "howtos/basics/publish-bot",
+        // "howtos/basics/publish-bot",
         "howtos/basics/access-management",
         "howtos/basics/add-faqs",
         "howtos/basics/compare-code",
