745355b3d04dc08a6dfca119cd337de6c1738163
Shree06-patel <patelshri73@gmail.com>
2021-07-28 11:02:15 +0530

    update footer image

--- src/core/Footer.js
+++ src/core/Footer.js
@@ -51,7 +51,7 @@ const Footer = () => {
     <footer className={styles.custom_footer}>
       <div className={styles.container}>
         <div className={styles.logo_container}>
-          <a href="https://yellow.ai/" target="_blank"><img src="img/Documentation.jpg" alt="logo" /></a>
+          <a href="https://yellow.ai/" target="_blank"><img src="img/yellowAi_white.svg" alt="logo" /></a>
           <p className={styles.description}>
             “The World’s Leading
             CX Automation Platform.”
