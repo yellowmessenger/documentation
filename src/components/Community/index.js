1a6690dd40969383c5a665cc3ad1f0405c42b0ac
Pratik Parmar <steveparmar6nov2011@gmail.com>
2022-01-13 15:09:40 +0530

    Fix community links in the footer

--- src/components/Community/index.js
+++ src/components/Community/index.js
@@ -4,12 +4,12 @@ import styles from "./community.module.css";
 const Community = () => {
   return (
     <div className={styles.community_wrapper}>
-      <h3>YM community</h3>
+      <h3>yellow.ai community</h3>
       <p>
         We always love to hear about projects being built on our platform. There
         is usually someone available to answer questions
       </p>
-      <a href="https://community.yellowmessenger.com/" target="_blank">
+      <a href="https://community.yellow.ai/" target="_blank">
         Ask the community
       </a>
     </div>
