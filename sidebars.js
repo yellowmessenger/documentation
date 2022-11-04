386c1c0be7faa18c888eb1451c5fbfda74cd723a
Monisha <monisha.s@yellow.ai>
2022-11-04 16:31:32 +0530

    sidebar-js for nice incontact

--- sidebars.js
+++ sidebars.js
@@ -727,6 +727,7 @@ module.exports = {
         "platform_concepts/appConfiguration/leadSquared-wa-connector",
         "platform_concepts/appConfiguration/microsoft-dynamics",
         "platform_concepts/appConfiguration/microsoft-graph",
+        "platform_concepts/appConfiguration/nice-incontact",
         "platform_concepts/appConfiguration/netcore",
         "platform_concepts/appConfiguration/paytm",
         "platform_concepts/appConfiguration/payuBiz",
