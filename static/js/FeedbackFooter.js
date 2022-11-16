456e9974774135fc32017d4fa19d5967ec3285bd
Dhinesh KS <dhinesh.k@yellow.ai>
2022-11-16 17:23:18 +0530

    fix: minor script changes

--- static/js/FeedbackFooter.js
+++ static/js/FeedbackFooter.js
@@ -12,5 +12,7 @@ function renderFeedbackFooter() {
   const footer = document.getElementById("feedbackFooter");
   const feedbackFooterHTML =
     "<div id='feedbackFooter' style='padding-left: 48px; padding-bottom: 20px;'><h3>Was the article helpful?</h3><a style='padding: 10px; text-decoration: none;' href='#'><i class='ri-thumb-up-fill'></i></a><a style='padding: 10px; text-decoration: none;' href='#'><i class='ri-thumb-down-fill'></i></a></div>";
-  if (container && !footer) container.innerHTML += feedbackFooterHTML;
+  const div = document.createElement("div");
+  div.innerHTML = feedbackFooterHTML;
+  if (container && !footer) container.appendChild(div);
 }
