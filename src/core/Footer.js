148703d0700790068b406607b50c5d0b0bce85d1
harshayellow <110393988+harshayellow@users.noreply.github.com>
2023-04-27 17:37:03 +0530

    minor footer changes

--- src/core/Footer.js
+++ src/core/Footer.js
@@ -66,8 +66,7 @@ const Footer = () => {
         <div className={styles.logo_container}>
           <a href="https://yellow.ai/" target="_blank"><img src="img/yai-light-mode.svg" alt="logo" /></a>
           <p className={styles.description}>
-            “The World’s Leading
-            CX Automation Platform.”
+            Autonomous customer & employee experience with human-like interactions.
           </p>
         </div>
         <div className={styles.list_container}>
