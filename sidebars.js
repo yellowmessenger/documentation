69ddba265f5efde2cd889d1ae0c13f26d81fcc86
Monisha <monisha.s@yellow.ai>
2023-07-24 15:47:52 +0530

    oracle responsys doc

--- sidebars.js
+++ sidebars.js
@@ -1144,6 +1144,7 @@ module.exports = {
             "platform_concepts/appConfiguration/microsoft-graph",
             "platform_concepts/appConfiguration/netcore",
             "platform_concepts/appConfiguration/okta",
+            "platform_concepts/appConfiguration/oracle-responsys",
             "platform_concepts/appConfiguration/outlook-calendar",
             "platform_concepts/appConfiguration/sftp",
             "platform_concepts/appConfiguration/twilio-verify",
