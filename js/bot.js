1dcd9fe4e4ca1d86a253807b67ef3cf494004023
Architha-j <architha.j@yellow.ai>
2023-09-08 16:45:45 +0530

    Deploy website - based on c050b7e7de671a426ee674ec1ff715ad6b8042c3

--- js/bot.js
+++ js/bot.js
@@ -1,12 +1,19 @@
 // script to integrate support bot
 
-const titlePathData = document.querySelectorAll('span.breadcrumbs__link');
-if (titlePathData.length) {
-    window.titleText = titlePathData[titlePathData.length-1].innerText
+const addDocTitleToPayload = () => {
+    const titlePathData = document.querySelectorAll('span.breadcrumbs__link');
+    if (titlePathData.length) {
+        const titleText = titlePathData[titlePathData.length-1].innerText
+        if (window.ymConfig.payload) {
+            window.ymConfig.payload.title = titleText;
+        } else {
+            window.ymConfig.payload = {title: titleText};
+        }
+    }
+    console.log("BOT.JS", window.ymConfig.payload)
 }
 
-
-window.ymConfig = {"bot":"x1687158407670","host":"https://r0.cloud.yellow.ai","payload": {"title": window.titleText}};
+window.ymConfig = {"bot":"x1687158407670","host":"https://r0.cloud.yellow.ai"};
 (function() {
   var w = window,
       ic = w.YellowMessenger;
@@ -21,12 +28,12 @@ window.ymConfig = {"bot":"x1687158407670","host":"https://r0.cloud.yellow.ai","p
           e.type = "text/javascript", e.async = !0, e.src = "https://cdn.yellowmessenger.com/plugin/widget-v2/latest/dist/main.min.js";
           var t = d.getElementsByTagName("script")[0];
           t.parentNode.insertBefore(e, t)
+          addDocTitleToPayload();
       }
       i.q = [], i.c = function(e) {
           i.q.push(e)
       }, w.YellowMessenger = i, w.attachEvent ? w.attachEvent("onload", l) : w.addEventListener("load", l, !1)}
 })();
-
 
 
 /* 
