596cc5843a3a71ab3e70c37f888372dbd33bef15
Architha-j <architha.j@yellow.ai>
2024-05-06 12:51:37 +0530

    Deploy website - based on 9bb927b72615759bbad1392e67c988387dc7c18a

---
+++ js/bot.js
@@ -0,0 +1,78 @@
+// script to integrate support bot
+
+/*const addDocTitleToPayload = () => {
+    const titlePathData = document.querySelectorAll('span.breadcrumbs__link');
+    if (titlePathData.length) {
+        const titleText = titlePathData[titlePathData.length-1].innerText
+        if (window.ymConfig.payload) {
+            window.ymConfig.payload.title = titleText;
+        } else {
+            window.ymConfig.payload = {title: titleText};
+        }
+    }
+    console.log("BOT.JS", window.ymConfig.payload)
+}
+*/
+
+window.ymConfig = {"bot":"x1696422652998","host":"https://r0.cloud.yellow.ai"};
+(function() {
+  var w = window,
+      ic = w.YellowMessenger;
+  if ("function" === typeof ic) ic("reattach_activator"), ic("update", ymConfig);
+  else {
+      var d = document,
+          i = function() {
+              i.c(arguments)
+          };
+      function l() {
+          var e = d.createElement("script");
+          e.type = "text/javascript", e.async = !0, e.src = "https://cdn.yellowmessenger.com/plugin/widget-v2/latest/dist/main.min.js";
+          var t = d.getElementsByTagName("script")[0];
+          t.parentNode.insertBefore(e, t)
+        //  addDocTitleToPayload();
+      }
+      i.q = [], i.c = function(e) {
+          i.q.push(e)
+      }, w.YellowMessenger = i, w.attachEvent ? w.attachEvent("onload", l) : w.addEventListener("load", l, !1)}
+})();
+
+
+/* 
+window.ymConfig = {
+    bot: "x1687158407670",
+   bot: "x1635319613183", 
+    host: "https://r0.cloud.yellow.ai",
+    partiallyOpen: false,
+    alignRight: "right",
+};
+(function () {
+    var w = window,
+        ic = w.YellowMessenger;
+    if ("function" === typeof ic)
+        ic("reattach_activator"), ic("update", ymConfig);
+    else {
+        var d = document,
+            i = function () {
+                i.c(arguments);
+            };
+        function l() {
+            var e = d.createElement("script");
+            (e.type = "text/javascript"),
+                (e.async = !0),
+                (e.src =
+                    "https://cdn.yellowmessenger.com/plugin/widget-v2/latest/dist/main.min.js");
+            var t = d.getElementsByTagName("script")[0];
+            t.parentNode.insertBefore(e, t);
+        }
+        (i.q = []),
+            (i.c = function (e) {
+                i.q.push(e);
+            }),
+            (w.YellowMessenger = i),
+            w.attachEvent
+                ? w.attachEvent("onload", l)
+                : w.addEventListener("load", l, !1);
+    }
+})();
+*/
+// window.ymConfig = {"bot":"x1635319613183","host":"https://cloud.yellowmessenger.com","partiallyOpen":false,"alignRight":"right"};(function () {var w=window,ic=w.YellowMessenger;if("function"===typeof ic)ic("reattach_activator"),ic("update",ymConfig);else{var d=document,i=function(){i.c(arguments)};function l(){var e=d.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://cdn.yellowmessenger.com/plugin/widget-v2/latest/dist/main.min.js";var t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}i.q=[],i.c=function(e){i.q.push(e)},w.YellowMessenger=i,w.attachEvent?w.attachEvent("onload",l):w.addEventListener("load",l,!1)}})();
