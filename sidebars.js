7546d0e9c30be6a2d6e1909e6369e2d0da40a7fc
Architha-j <architha.j@yellow.ai>
2025-03-26 23:18:20 +0530

    +1

--- sidebars.js
+++ sidebars.js
@@ -74,6 +74,7 @@ module.exports = {
           "cookbooks/studio/debug-logs",
           "cookbooks/studio/error_page",
           "cookbooks/studio/userinitiateddisconnection",
+          "cookbooks/studio/qr-dc",
 
       ],
     }, 
